87be74fb6626c2a3d990025b519caf5fe201d27c
VikramKumar-1 <vikuraj209@gmail.com>
2026-06-04 11:35:32 +0530

    Configure Cloudinary image domains for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,16 @@ try {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
